a091b399993e517d1be18127e11ae9ea1ae80f6a
Ama <85511496+amaboh@users.noreply.github.com>
2023-10-29 12:09:02 +0000

    init book scrape for basics

--- equityscraper/equityscraper/spiders/equityspider.py
+++ equityscraper/equityscraper/spiders/equityspider.py
@@ -3,24 +3,24 @@ import scrapy
 
 class EquityspiderSpider(scrapy.Spider):
     name = "equityspider"
-    allowed_domains = ["ngxgroup.com"]
-    start_urls = ["https://ngxgroup.com/exchange/data/equities-price-list/"]
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/']
 
     def parse(self, response):
-        # Get all rows from the table 
-        rows = response.xpath('//tbody[@id="ngx_equities_trading_statistics"]//tr')
-        for row in rows:
+        # Get all rows from the table
+        books = response.css('article.product_pod')
+        for book in books:
             yield {
-            'Company': row.xpath('./td[1]/text()').get(),
-            'Previous Closing Price': row.xpath('./td[2]/text()').get(),
-            'Opening Price': row.xpath('./td[3]/text()').get(),
-            'High': row.xpath('./td[4]/text()').get(),
-            'Low': row.xpath('./td[5]/text()').get(),
-            'Close': row.xpath('./td[6]/text()').get(),
-            'Change': row.xpath('./td[7]/text()').get(),
-            'Trades': row.xpath('./td[8]/text()').get(),
-            'Volume': row.xpath('./td[9]/text()').get(),
-            'Value': row.xpath('./td[10]/text()').get(),
-            'Trade Date': row.xpath('./td[11]/text()').get(),
-        }
+                'name': book.css('h3 a::text').get(),
+                'price': book.css('div .product_price .price_color::text').get(),
+                'url': book.css(' h3 a').attrib['href'],
+            }
 
+        next_page = response.css('li.next a ::attr(href)').get()
+
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
